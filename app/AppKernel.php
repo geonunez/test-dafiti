75e0ec310307946367fff0b89f54082d53ce7d75
Jorge Nuñez <jorge.nunez@geonunez.com>
2017-07-28 16:43:23 -0300

    Created Bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Dafiti\AdminBundle\DafitiAdminBundle(),
+            new Dafiti\WebBundle\DafitiWebBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/Dafiti/AdminBundle/DafitiAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dafiti\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DafitiAdminBundle extends Bundle
+{
+}
